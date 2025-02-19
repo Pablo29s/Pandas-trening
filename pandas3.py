72869dc18aea8a89aadfe67a123f8083f9405193
Paweł <pawel.szymala707@gmail.com>
2025-02-19 18:36:40 +0100

    Coś

--- pandas3.py
+++ pandas3.py
@@ -9,17 +9,23 @@ df["Wielkość_Kraju"] = 0
 df['land area km'] = df['land area km'].apply(str) + ' km²'
 
 
-for index, row in df.iterrows():
-    if row['population'] > 50000000:
-        df.loc[index, 'Wielkość_Kraju'] = 'Ogromne'
-    elif row['population'] > 10000000:
-        df.loc[index, 'Wielkość_Kraju'] = 'Duże'
-    elif row['population'] > 1000000:
-        df.loc[index, 'Wielkość_Kraju'] = 'Średnie'
-    elif row['population'] < 1000000:
-        df.loc[index, 'Wielkość_Kraju'] = 'Małe'
+for row in df.itertuples():
+    if row.population > 50000000:
+        print(f"{row.Index} Ogromny kraj: {row.name}")
+        print("Populacja: ", df.loc[row.Index, "population"])
+        print("--------------------------------------------------")
+    if row.population > 10000000:
+        print(f"{row.Index} Ogromny kraj: {row.name}")
+        print("Populacja: ", df.loc[row.Index, "population"])
+        print("--------------------------------------------------")
+    if row.population > 1000000:
+        print(f"{row.Index} Średni kraj: {row.name}")
+        print("Populacja: ", df.loc[row.Index, "population"])
+        print("--------------------------------------------------")
+    if row.population < 1000000:
+        print(f"{row.Index} Mały kraj: {row.name}")
+        print("Populacja: ", df.loc[row.Index, "population"])
+        print("--------------------------------------------------")
 
-df['population'] = ((df['population'] / 1000000).round(2)).apply(str) + ' mln'
-print(df[df['Wielkość_Kraju'] == 'Małe'])
 
-
+    
