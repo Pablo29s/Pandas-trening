896bfe40b7941fe215197deaee4975308e2246b4
Paweł <pawel.szymala707@gmail.com>
2025-02-20 15:22:16 +0100

    Zmiany

--- pandas3.py
+++ pandas3.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('car_price_dataset.csv')
+a = [["Karmel", 8], ["Tofik", 1], ["Szary", 2]]
+b = {"Imię": ["Biało-czarny", "Karmelczyk", "Mruczek"], "Wiek": [3, 2, 5], "Rasa": ["Rasowy", "Mieszany", "Zwykły"]}
 
+df1 = pd.DataFrame(a)
+df2 = pd.DataFrame(b)
 
-print(df.groupby(['Year', 'Brand']).size())
+print(df1.plot(kind='bar'))
