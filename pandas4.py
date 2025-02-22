de0c309f2da16bbeb0a8ba22c986443e0104509d
Paweł <pawel.szymala707@gmail.com>
2025-02-22 21:58:58 +0100

    Zmiany

--- pandas4.py
+++ pandas4.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 cena = input("Cena samochodu (Niska, Wysoka, Średnia, Bardzo niska): \n")
 transmission = input("Rodzaj skrzyni biegów (Automatic, Manual, Semi-Automatic): \n")
 year = int(input("Rok produkcji (od 2000 do obecnego): \n"))
+fuel = input("Rodzaj paliwa (Diesel, Electric, Hybrid, Petrol): \n")
 
 
 
@@ -18,11 +20,11 @@ df.loc[(df["Price"] >= 5500) & (df["Price"] < 8500), "Cena"] = "Niska"
 df.loc[df["Price"] < 5500, "Cena"] = "Bardzo niska"
 
 
-filtered_df = df[(df["Cena"] == cena) & (df["Transmission"] == transmission) & (df["Year"] == year)]
+filtered_df = df[(df["Cena"] == cena) & (df["Transmission"] == transmission) & (df["Year"] == year) & (df["Fuel_Type"] == fuel)]
 
 
 if not filtered_df.empty:
-    print("\nIdealne samochody:")
+    print("\nIdealne samochody :")
     print(filtered_df[['Brand', 'Model']].to_string(index=False))
 else:
     print("\nBrak samochodów spełniających kryteria.")
